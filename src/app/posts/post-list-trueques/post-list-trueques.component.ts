2396cc27f4b2083414ca7d6645f801eca28d11d5
joseDeuteros <josewbmx@gmail.com>
2018-02-11 03:15:05 -0600

    Se agregaron los estilos para trueques

--- src/app/posts/post-list-trueques/post-list-trueques.component.ts
+++ src/app/posts/post-list-trueques/post-list-trueques.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef} from '@angular/core';
 import { Post } from '../post';
 import { PostsService } from '../posts.service';
 import { Router } from '@angular/router';
 import { trigger, state, style, animate,transition } from '@angular/animations';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 
 @Component({
@@ -41,7 +43,7 @@ export class PostListTruequesComponent implements OnInit {
 
 
 
- constructor( private postsService: PostsService, private router: Router) {
+ constructor( private postsService: PostsService, private router: Router, private sanitizer: DomSanitizer, elm: ElementRef) {
 
     this.post_count = 20;
     this.arregloCountCat = [];
@@ -139,6 +141,8 @@ GetPosts_trueques(){
 }
 
 
-
+  getBackground(image) {
+    return this.sanitizer.bypassSecurityTrustStyle(`linear-gradient(rgba(29, 29, 29, 0), rgba(16, 16, 23, 0.5)), url(${image})`);
+  }
 
 }
