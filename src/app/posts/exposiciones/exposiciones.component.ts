ae35e0fd393d69f13cc79e82af712aaf4888fd9e
joseDeuteros <josewbmx@gmail.com>
2018-02-11 03:20:10 -0600

    Se agregaron los nuevos estilos para exposiciones

--- src/app/posts/exposiciones/exposiciones.component.ts
+++ src/app/posts/exposiciones/exposiciones.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef} from '@angular/core';
 import { Post } from '../post';
 import { PostsService } from '../posts.service';
 import { Router } from '@angular/router';
 import { trigger, state, style, animate,transition } from '@angular/animations';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-exposiciones',
@@ -38,7 +40,7 @@ export class ExposicionesComponent implements OnInit {
 
 
 
-  constructor(private postsService: PostsService, private router: Router) { 
+  constructor(private postsService: PostsService, private router: Router, private sanitizer: DomSanitizer, elm: ElementRef) { 
     this.post_count = 20;
     this.arregloCountCat = [];
     this.loadMoreVisible = false
@@ -135,6 +137,10 @@ export class ExposicionesComponent implements OnInit {
    //this.router.navigate([slug]);
     console.log("Slug normal", slug)
 }
+
+  getBackground(image) {
+    return this.sanitizer.bypassSecurityTrustStyle(`linear-gradient(rgba(29, 29, 29, 0), rgba(16, 16, 23, 0.5)), url(${image})`);
+  }
 
 
 }
